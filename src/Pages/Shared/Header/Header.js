023201740dfb164e58808d495566456e06dc3504
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-10-15 22:14:57 +0600

    firebase deploy

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 import useAuth from '../../../hooks/useAuth';
+
 
 const Header = () => {
       const {user, logOut} = useAuth();
@@ -12,9 +14,9 @@ const Header = () => {
                               <Navbar.Brand className="text-primary fw-bold" href="#home">Car kit 24/7</Navbar.Brand>
                               <Navbar.Toggle />
                               <Navbar.Collapse className="justify-content-end">
-                                    <Nav.Link as={Link} to="/home#home">Home</Nav.Link>
-                                    <Nav.Link as={Link} to="/home#services">Services</Nav.Link>
-                                    <Nav.Link as={Link} to="/home#experts">Experts</Nav.Link>
+                                    <Nav.Link as={HashLink} to="/home#home">Home</Nav.Link>
+                                    <Nav.Link as={HashLink} to="/home#services">Services</Nav.Link>
+                                    <Nav.Link as={HashLink} to="/home#experts">Experts</Nav.Link>
                                    { 
                                    user.email ?
                                    <Button onClick={logOut} variant="primary" className="rounded-pill px-2 py-1 me-2">Sign out</Button>:
